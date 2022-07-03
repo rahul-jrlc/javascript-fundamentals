6d1b46fcdb3abe13ce8380903dd47104641b4c3b
Rahul <rahul.pandita41299@gmail.com>
2022-07-03 20:27:21 +0530

    Helper methods

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-document.querySelectorAll(".button").forEach((item) => {
-        item.addEventListener("click", () => {
-          alert("AWESOME");
-        });
-      });
+// HELPER METHODS
+// DOCUMENT.WRITE("HELLO")
+// ALERT("HELLO")
+// CONSOLE.LOG("HELLO")
+
+document.write("hello world how are you") // WRITES SOME EXPRESSION TO HTML DOC, SHOWN ON WEBPAGE
+alert("hello people")
+console.log("hello world")
