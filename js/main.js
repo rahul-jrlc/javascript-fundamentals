1a3b183e771bc69645cdc48b5369886ce20ca0c6
Rahul <rahul.pandita41299@gmail.com>
2022-07-07 16:54:44 +0530

    Numbers- basic math operations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-// STRING CONCATENATION -- COMBINE STRING VALUES
-// `` - BACKTICKS-- TEMPLATE STRINGS
+// NUMBERS
+// LOOSELY TYPED = DON'T DECLARE TYPE IN JAVASCRIPT
 
-// const name = "john";
-// const lastName = "blahblah";
-// let fullName = name + ' ' + lastName
+const num = 34;
+// let num2 = 2.443;
+// num2 = 'helloooo';
 
-// console.log("Hello there your full name is : " + fullName);
+const num2 = 2.344;
+console.log(num2);
 
-const website = "google";
-const url = "https://www." + website + ".com";
-console.log(url);
+const num3 = "2.344";
+console.log(num3);
+
+const add = num + num2;
+const sub = num - num2;
+const mult = num * num2;
+const div = num / num2;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
