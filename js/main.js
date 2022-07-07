6cfba1b7a70794228dce0d7f698087d315e460e7
Rahul <rahul.pandita41299@gmail.com>
2022-07-07 19:13:16 +0530

    Functions- declare, invoke

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 // OBJECT DATA TYPES- ARRAYS, FUNCTIONS, OBJECTS
-// ARRAYS - [], 0 INDEX BASED
+// FUNCTIONS- DECLARE, INVOKE
 
-const friend1 = "tinku";
-const friend2 = "tinku";
-const friend3 = "tinku";
-const friend4 = "tinku";
+function hello() {  // DECLARED A FUNCTION
+    // logic
+    console.log("bob");
+    console.log("ana");
+    console.log("tinku");
+}
 
-const friends = ["john", "peter", "meenu", "tinku", 47, undefined, null];
+hello();  // INVOKING A FUNCTION
 
-let bestFriend = friends[2];
 
-friends[4] = 'veeru';  // CAN REASSIGN VALUES BASED ON INDEXES
-console.log(friends);
-console.log(friends[4]);
 
-console.log(bestFriend);
-
-console.log(friends[0]);
+// console.log("bob");
+// console.log("ana");
+// console.log("tinku");
+// // some code here
+// console.log("bob");
+// console.log("ana");
+// console.log("tinku");
+// // some code here
+// console.log("bob");
+// console.log("ana");
+// console.log("tinku");
+// // some code here
