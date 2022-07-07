102e77b9dcee975aeec976e61bc198913d4fa17f
Rahul <rahul.pandita41299@gmail.com>
2022-07-07 15:28:55 +0530

    Variables-- add value later, modify existing value

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 // VARIABLES - MOST BASIC BUILDING BLOCK
 // VARIABLES - STORE, ACCESS, MODIFY === VALUE
 // DECLARE, ASSIGNMENT OPERATOR, ASSIGN VALUE
+// ASSIGN VALUE LATER, MODIFY EXISTING
 
 let firstName = "eenu meenu peenu";
-console.log(firstName);
+let address, zipCode, state; // DECLARES VARIABLES, UNDEFINED
+console.log(state);
+
+address = "101 second street" // ASSIGN VALUE LATER
+console.log(address);
+zipCode = 12232
+state = "Haryana"
+
+console.log(firstName, zipCode, address);
+
+firstName = "Chinku" // MODIFY EXISTING VARIABLE VALUE
+console.log(firstName)
