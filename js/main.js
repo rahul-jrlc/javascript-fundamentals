3ad509b53a5f94ed463600f560cedb03382b06af
Rahul <rahul.pandita41299@gmail.com>
2022-07-08 18:43:38 +0530

    switch statements

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
-// CONDITIONAL STATEMENTS
-// >, <, >=, <=, ==, ===, !=, !===
-// EQUALITY
-// == CHECKS ONLY VALUE
-// CHECKS VALUE AND TYPE
+// SWITCH STATEMENT
+// DICE VALUES : 1-6
 
-const num1 = 6;
-const num2 = '6';
+const dice = 2;
 
-const value = num1 == num2;  // CHECKS ONLY VALUE
-const value2 = num1 === num2; // CHECKS VALUE AND TYPE
+switch (dice) {
+    case 1:
+        console.log("got one");
+        break
+    case 2:
+        console.log("got two");
+        break
+    case 3:
+        console.log("got three");
+        break
+    default:
+        console.log("no roll");
+}
 
-console.log(value);
-console.log(value2);
+
+// if (dice === 1) {
+//     console.log("you got one");
+// } else if (dice === 2) {
+//     console.log("got 2");
+// } else {
+//     console.log('not roll');
+// }
+
+
+
+// if (dice === 1) {
+//     console.log("you got one");
+// }
+// if (dice === 2) {
+//     console.log("got two");
+// }
+// if (dice < 1 || dice > 6) {
+//     console.log('did not roll the dice');
+// } 
