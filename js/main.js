66c9b3841a7cc31e60bc2233e97f7c7e272e83f2
Rahul <rahul.pandita41299@gmail.com>
2022-07-09 19:44:45 +0530

    Higher order functions, callback functions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,57 @@
-// VARIABLE LOOKUP
-// {} - CODE BLOCK
+function morning(name) {
+    // console.log(`good morning bob`);
 
-//DOES NOT WORK THE OPPOSITE WAY
-
-const globalNumber = 5; // GLOBAL SCOPE
-
-function addNum(num1, num2) {
-    const globalNumber = 100;
-    const result = num1 + num2 + globalNumber; // ENCLOSING SCOPE
-    function multiply() {
-        const globalNumber = 0; // LOCAL SCOPE
-        const multiplyResult = result * globalNumber;
-        console.log(multiplyResult);
-    }
-    multiply()
-    return result;
+    return `good morning ${name.toUpperCase()}`;
 }
 
-console.log(addNum(3, 10));
+function afternoon(name) {
+    return `good afternoon ${name.repeat(3)}`
+}
+
+function greet(name, callback) {
+    // callback()
+    const myName = "john";
+    console.log(`Hello ${callback(name)}, my name is ${myName}`);
+    
+}
+
+greet("pinky", morning);
+greet("lulu", afternoon);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CALLBACK FUNCTIONS, HIGHER ORDER FUNCTIONS, FUNCTIONS AS FIRST CLASS OBJECTS/CITIZENS
+// FUNCTIONS ARE FIRST CLASS OBJECTS - STORED IN A VARIABLE(EXPRESSION), PASSED AS AN ARGUMENT TO ANOTHER FUNCTION, RETURN FROM THE FUNCTION (CLOSURE)
+
+// HIGHER ORDER FUNCTIONS - ACCEPTS ANOTHER FUNCTION AS AN ARGUMENT OR RETURNS ANOTHER FUNCTION AS A RESULT
+
+// CALLBACK FUNCTIONS - PASSED TO ANOTHER FUNCTION AS AN ARGUMENT AND EXECUTED INSIDE THAT FUNCTION
+
+
+
+
+// function greetMorning(name) {
+//     const myName = "john";
+//     console.log(`Good morning ${name}, my name is ${myName}`);
+// }
+
+// function greetAfternoon(name) {
+//     const myName = "john";
+//     console.log(`Good afternoon ${name}, my name is ${myName}`);
+// }
+
+// KEEP REPEATING 
