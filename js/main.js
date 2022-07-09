b775aa1f3c2ab2235d807c7342bede2bdd11b201
Rahul <rahul.pandita41299@gmail.com>
2022-07-09 18:00:53 +0530

    null and undefined

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-// REFERENCE V/S VALUE
-// PRIMITIVE DATA TYPES- STRING, NUMBER, BOOLEAN, UNDEFINED, NULL, SYMBOL
-// OBJECT DATA TYPES - ARRAY, FUNCTIONS, OBJECTS
-// TYPEOF
+// NULL AND UNDEFINED
+// BOTH REPRESENT NO VALUE
 
-// WHEN ASSIGNING PRIMITIVE DATA TYPE VALUE TO A VARIABLE ANY CHANGES MADE ARE MADE DIRECTLY TO THAT VALUE, WITHOUT AFFECTING ORIGINAL VALUE
+// UNDEFINED - "JAVASCRIPT CANNOT FIND VALUE FOR THIS"
 
-// WHEN ASSIGNING NON-PRIMITIVE DATA TYPE VALUE TO A VARIABLE IS DONE BY REFERENCE SO ANY CHANGES WILL AFFECT ALL REFERENCES.
+// VARIABLE WITHOUT VALUE
+// MISSING FUNCTION ARGUMENTS
+// MISSING OBJECT PROPERTIES
 
-const num = 1;
-let num2 = num;
-num2 = 7;
-console.log(`first value is ${num}`);
-console.log(`second value is ${num2}`);
+// NULL - DEVELOPER SETS THE VALUE
 
-let person = {
-    name: 'bob'
-};
-let person2 = person;
+let num = 20 + null; // 20 + 0;
+console.log(num);
 
-person2.name = 'pinky';
-
-console.log(`the name of the first person is ${person.name}`);
-console.log(`the name of the second person is ${person2.name}`);
+let num2 = 20 + undefined; // 20 + 0;
+console.log(num2);
