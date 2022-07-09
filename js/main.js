bf0810e3d97fb32a4f324c4285bcf8454ccf4ad9
Rahul <rahul.pandita41299@gmail.com>
2022-07-09 16:48:02 +0530

    Array methods and properties

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-// TEMPLATE LITERALS - ES6+
+// ARRAY PROPERTIES AND METHODS
 
-const firstName = "john";
-const age = 33;
-const sentence = "My name is " + firstName + " and I am " + age + " years old.";
-console.log(sentence);
+let names = ["john", "bob", "tinku", "pinky", "sujata"];
 
-const sentence2 = `My name is ${firstName} and I'm ${age} years old.`;
-console.log(sentence2);       // INTERPOLATION - INSERT EXPRESSION(VALUE)
+// LENGTH
+console.log(names.length); // PROPERTY
+console.log(names[3]);
+console.log(names[names.length - 1]); // RETURNS LAST ITEM IN ARRAY
+
+// CONCAT -- COMBINE TWO ARRAYS
+const lastNames = ["lulu", "tulu", "bravo", "gupta"];
+const allNames = names.concat(lastNames);
+console.log(allNames);
+
+// REVERSE ARRAY
+console.log(allNames.reverse());
+console.log(allNames);  // MODIFIES EXISTING ARRAY
+
+// UNSHIFT - ADD TO BEGINNING OF ARRAY
+allNames.unshift("jojojo"); // RUNS IN PLACE, MODIFIES ARRAY
+console.log(allNames);
+
+// SHIFT - REMOVES FIRST ITEM FROM ARRAY
+allNames.shift();
+console.log(allNames);
+console.log(allNames.shift()); // RETURNS THE SHIFTED ITEM
+
+// PUSH - APPENDS NEW ELEMENTS TO END OF ARRAY
+allNames.push('NEW ELEMENT')
+console.log(allNames.push("NEW ELEMENT 2")); // RETURNS MODIFIED ARRAY LENGTH
+console.log(allNames);
+
+// POP -- REMOVES ITEM FROM END OF ARRAY
+allNames.pop();
+console.log(allNames);
+console.log(names.pop()); // RETURNS POPPED ITEM
+
+// SPLICE - GRAB SPECIFIC ITEMS FROM ARRAY -- MUTATES ORIGINAL ARRAY
+allNames.splice(2); // START, NUMBER OF ELEMENTS TO REMOVE
+console.log(allNames);
+
