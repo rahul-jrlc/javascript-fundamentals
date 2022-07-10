34da1d8476921da241cd3323454e95fef8fddfde
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 17:38:39 +0530

    query selector and query selector all

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-// GET ELEMENTS BY CLASS NAME
 // NODE-LIST IS AN ARRAY LIKE OBJECT
 // INDEX, LENGTH PROPERTY BUT NOT ARRAY METHODS
 
-const listItems = document.getElementsByClassName("special");
-listItems[1].style.color = "blue";
+// QUERY SELECTOR - SELECTS SINGLE
+// QUERY SELECTOR ALL - SELECTS ALL - CAN USE FOR EACH
+
+
+const result = document.querySelector("#result");
+result.style.background = "blue";
+
+const item = document.querySelector(".special"); // SELECTS FIRST INSTANCE OF QUERY(CLASS)
+console.log(item);
+
+const lastItem = document.querySelector("li:last-child"); // SELECTS LAST CHILD OF LI
+console.log(lastItem);
+
+const list = document.querySelectorAll(".special");// SELECTS EVERY INSTANCE OF QUERY(CLASS), PUTS INSIDE NODE LIST
+console.log(list);
+
+list.forEach(function (item) {
+    console.log(item);
+    item.style.color = "yellow";
+})
