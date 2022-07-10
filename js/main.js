36c73eb0062d79199c9b224adaa1eabafb88cfca
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 16:57:55 +0530

    DOM general concepts

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// DATE
-const months = ["Jan", "feb", "march", 'a', 'm', 'j', 'ju', 'aug', 'sep', 'oc', 'n', 'd'];
-const date = new Date()
-const month = date.getMonth();
-console.log(months[month]);
+// DOM - DOCUMENT OBJECT MODEL
+
+document.body.style.background = "blue"; // SELECTING BODY FROM DOC AND APPLYING STYLE
+document.body.style.color = "red";
+document.getElementById("button").style.color = "pink";
+
+document.querySelector(".button").style.padding = '100px';
+
+// WINDOW OBJECT
+console.log(window);
+
+//RETURNS A NODE OBJECT OR A NODE LIST, WHICH IS AN ARRAY LIKE OBJECT
+
+const button = document.getElementById("button");
+const name = button.nodeName;
+const css = button.style;
+
+console.log(button);
+console.log(name);
+console.log(css);
