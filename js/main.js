b09be8945f90fc4c357e9b7e9ead1613b1d9f2ee
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 17:08:14 +0530

    window and document overview

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
-// DOM - DOCUMENT OBJECT MODEL
+// WINDOW OBJECT = BROWSER API
+// DOCUMENT - object in window object
+// CONSOLE.DIR
 
-document.body.style.background = "blue"; // SELECTING BODY FROM DOC AND APPLYING STYLE
-document.body.style.color = "red";
-document.getElementById("button").style.color = "pink";
-
-document.querySelector(".button").style.padding = '100px';
-
-// WINDOW OBJECT
-console.log(window);
-
-//RETURNS A NODE OBJECT OR A NODE LIST, WHICH IS AN ARRAY LIKE OBJECT
-
-const button = document.getElementById("button");
-const name = button.nodeName;
-const css = button.style;
-
-console.log(button);
-console.log(name);
-console.log(css);
+console.log(document);
+console.dir(document);
