aea840b954fc492860e9a83709cd310f130ed9ea
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 17:48:06 +0530

    navigate the dom - children(child nodes)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
 // NODE-LIST IS AN ARRAY LIKE OBJECT
 // INDEX, LENGTH PROPERTY BUT NOT ARRAY METHODS
 
-// QUERY SELECTOR - SELECTS SINGLE
-// QUERY SELECTOR ALL - SELECTS ALL - CAN USE FOR EACH
-
+// CHILD NODES - RETURNS ALL CHILD NODES INCLUDING WHITESPACE WHICH IS TREATED AS A TEXT NODE
+// CHILDREN - RETURNS ACTUAL CHILDREN
+// FIRST CHILD
+// LAST CHILD
 
 const result = document.querySelector("#result");
-result.style.background = "blue";
+const allChildren = result.childNodes;
+// console.log(allChildren);
 
-const item = document.querySelector(".special"); // SELECTS FIRST INSTANCE OF QUERY(CLASS)
-console.log(item);
+const children = result.children; // RETURNS ACTUAL CHILDREN, NO WHITESPACE
+console.log(children);
 
-const lastItem = document.querySelector("li:last-child"); // SELECTS LAST CHILD OF LI
-console.log(lastItem);
-
-const list = document.querySelectorAll(".special");// SELECTS EVERY INSTANCE OF QUERY(CLASS), PUTS INSIDE NODE LIST
-console.log(list);
-
-list.forEach(function (item) {
-    console.log(item);
-    item.style.color = "yellow";
-})
+console.log(result.firstChild);
+console.log(result.lastChild);
