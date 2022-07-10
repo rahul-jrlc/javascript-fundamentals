6d14d484df38720794bd90cd4fd4fa50a14f7837
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 14:45:59 +0530

    reduce method

--- js/main.js
+++ js/main.js
@@ -15,29 +15,24 @@
 // DOES NOT RETURN NEW ARRAY
 
 const people = [
-    { name: "lulu", age: 20, position: "developer", id: 1 },
-    { name: "pinky", age: 33, position: "designer", id: 2 },
-    { name: "montu", age: 77, position: "ceo", id: 3 }
+    { name: "lulu", age: 20, position: "developer", id: 1, salary: 200 },
+    { name: "pinky", age: 33, position: "designer", id: 2 , salary: 100},
+    { name: "montu", age: 77, position: "ceo", id: 3, salary: 500 }
 ];
 
-// FIND
-// RETURNS SINGLE INSTANCE -(IN THIS CASE, AN OBJECT)
-// RETURNS FIRST MATCH, IF NO MATCH UNDEFINED
-// GREAT FOR GETTING UNIQUE VALUES
+// REDUCE
+// ITERATES, CALLBACK FUNCTION
+// REDUCES TO A SINGLE VALUE - NUMBER, ARRAY, OBJECT
+// 1 PARAMETER ('ACC') - TOTAL OF ALL CALCULATIONS
+// 2 PARAMETER ('CURR') - CURRENT ITERATION/VALUE
 
-const person = people.find(function (person) {
-    return person.id === 2;
-})
 
-console.log(person);
+// RETURN VALUE IS THE ACCUMULATED RESULT, 
+const total = people.reduce(function (acc, currItem) {
+    console.log(`total: ${acc}`);
+    console.log(`current money: ${currItem.salary}`);
+    acc += currItem.salary
+    return acc;
+}, 0) // SECOND ARGUMENT 0 IS THE ACC STARTING VALUE
 
-const names = ["kuku", "pintu", "bunny", "kaku"];
-console.log(names.find(function(name) {
-    return name === "chintu";  // UNDEFINED
-}));
-
-const person2 = people.filter(function (person) {
-    return person.id === 2;
-})
-// FILTER WILL RETURN AN ARRAY
-console.log(person2);
+console.log(total);
