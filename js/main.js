d3f4bdc8145da3e9ff78aa91393a76d169cd62a7
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 17:29:55 +0530

    get elements by class name

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
-// GET ELEMENTS BY TAG NAME
+// GET ELEMENTS BY CLASS NAME
 // NODE-LIST IS AN ARRAY LIKE OBJECT
 // INDEX, LENGTH PROPERTY BUT NOT ARRAY METHODS
 
-const headings = document.getElementsByTagName("h2"); // RETURNS A NODE LIST CONTAINING ALL H2
-// console.log(headings); // ARRAY LIKE BUT NOT EXACTLY
-headings[0].style.color = "red";
-// console.log(headings.length);
-
-const items = document.getElementsByTagName("li");
-
-// items.forEach(function (item) { // WILL NOT WORK, NOT AN ARRAY
-//     console.log(item);
-// })
-
-items[2].style.color = 'blue';
+const listItems = document.getElementsByClassName("special");
+listItems[1].style.color = "blue";
