45101549a54511f672bb76cbb2258bfdef769c45
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 15:01:10 +0530

    date object

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-// MATH
-// STANDARD BUILT-IN OBJECTS - ALWAYS AVAILABLE
-
-
-// const num = 4.3422988;
-// const result = Math.floor(num);
-
-// const result = Math.ceil(2.000000001)
-
-// const result = Math.sqrt(100);
-
-// const result = Math.PI;
-
-// const result = Math.min(2, 99, 10000, 0, -2);
-
-// const result = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 99999, -1);
-
-
-// const result = Math.random() * 10;
-const result = Math.floor((Math.random() * 10) + 1)
-console.log(result);
+// DATE
+const months = ["Jan", "feb", "march", 'a', 'm', 'j', 'ju', 'aug', 'sep', 'oc', 'n', 'd'];
+const date = new Date()
+const month = date.getMonth();
+console.log(months[month]);
