2ac8b03b2738d91f8526aa5addf1e7db74ef874d
Rahul <rahul.pandita41299@gmail.com>
2022-07-10 17:17:55 +0530

    get element by id

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-// WINDOW OBJECT = BROWSER API
-// DOCUMENT - object in window object
-// CONSOLE.DIR
+// GET ELEMENT BY ID
 
-console.log(document);
-console.dir(document);
+const h1 = document.getElementById("title"); // GIVES ACCESS TO NODE OBJECT
+h1.style.color = "red";
+
+// document.getElementById("button").style.background = "blue";
+// document.getElementById("button").style.color = "white";
+
+const button = document.getElementById("button");
+button.style.background = "blue";
+button.style.color = "white";
