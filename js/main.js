ea50dc9ff069fcb9ef7754a36883706a22d72e6b
Rahul <rahul.pandita41299@gmail.com>
2022-07-11 12:51:25 +0530

    inner text and prepend

--- js/main.js
+++ js/main.js
@@ -1,32 +1,7 @@
-// CREATE ELEMENT("ELEMENT")
-// CREATE TEXT NODE("TEXT")
-// APPEND CHILD
-// INSERT BEFORE ("ELEMENT", "LOCATION")
-
-const result = document.querySelector("#result");
-
-// create empty element
-
-const bodyDiv = document.createElement("div");
-
-// create text node
-const text = document.createTextNode("a simple body div");
-
-//append text node to body div
-bodyDiv.appendChild(text);
-
-// APPEND TO BODY IN HTML DOC
-// document.body.appendChild(bodyDiv);
-document.body.insertBefore(bodyDiv, result);
+// PREPEND
+// INNER TEXT
 
 const heading = document.createElement("h2");
-const headingText = document.createTextNode("Dynamic heading");
-heading.appendChild(headingText);
-heading.classList.add("blue");
-// result.appendChild(heading);
+heading.innerText = "i am a dynamic heading";
 
-console.log(result.children);
-
-// REPLACE CHILD (OLD , NEW)
-
-document.body.replaceChild(h1, heading);
+document.body.prepend(heading);
