4d524faa6a82a030e40f0d372a4d470e50bc45bd
Rahul <rahul.pandita41299@gmail.com>
2022-07-11 13:53:40 +0530

    callback function as a reference

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 // SELECT ELEMENT
 // ADD EVENT LISTENER
 // WHAT EVENT, WHAT TO DO
+// FUNCTION REFERENCE
 
 const button = document.querySelector(".button");
 const heading = document.querySelector("h2");
 
-button.addEventListener("click", function () {
-    console.log("button clicked");
-    heading.classList.add("red");
-})
+function changeColors() {
+    let hasClass = heading.classList.contains("red"); // EITHER TRUE OR FALSE
+    if (hasClass) {
+        heading.classList.remove("red");
+    } else {
+        heading.classList.add("red");
+    }
+
+};
+
+button.addEventListener("click", changeColors); // CALLBACK FUNCTION AS A REFERENCE
