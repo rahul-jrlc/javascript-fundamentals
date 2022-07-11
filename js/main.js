a0a591d179e9d28809651d7608db8ff852050982
Rahul <rahul.pandita41299@gmail.com>
2022-07-11 14:22:19 +0530

    key events

--- js/main.js
+++ js/main.js
@@ -1,32 +1,19 @@
-// CLICK - FIRES AFTER FULL ACTION OCCURS
-// MOUSE EVENTS
-// MOUSEUP - BUTTON IS PRESSED
-// MOUSEDOWN - BUTTON IS RELEASED
-// MOOUSEENTER - MOVED ONTO AN ELEMENT
-// MOUSELEAVE - MOVED OUT OF AN ELEMENT
+// KEY EVENTS
+// KEY PRESS - WHEN KEY IS PRESSED
+// KEY DOWN - WHEN KEY IS DOWN
+// KEYUP - WHEN KEY IS RELEASED
 
-const heading = document.querySelector("h2");
-const button = document.querySelector(".button");
+const nameInput = document.getElementById("name");
 
-button.addEventListener("click", function () {
-    console.log("button clicked");
+// nameInput.addEventListener("keypress", function () {
+//     console.log(("pressed a key"));
+// })
+
+// nameInput.addEventListener("keydown", function () {
+//     console.log(("pressed a key"));
+// });
+
+nameInput.addEventListener("keyup", function () {
+    // console.dir(nameInput);
+    console.log(nameInput.value);
 });
-
-button.addEventListener("mousedown", function () {
-    console.log("mouse down");
-});
-
-button.addEventListener("mouseup", function () {
-    console.log("mouse up");
-});
-
-const random = document.querySelector(".random");
-random.classList.add("design")
-
-random.addEventListener("mouseenter", function () {
-    random.style.background = "black";
-    random.style.height = "200px";
-});
-
-// SET UP ANOTHER CALLBACK FUNCTION FOR MOUSE LEAVE
-
