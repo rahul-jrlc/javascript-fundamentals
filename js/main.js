fb0049b12109901446bb718197d6509e60efc5e6
Rahul <rahul.pandita41299@gmail.com>
2022-07-12 19:16:36 +0530

    OOP - Encapsulation

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-// JSON.STRINGIFY
-// JSON.PARSE
+// OOP
+// COMBINE GROUP OF RELATED VARIABLES AND FUNCTIONS IN A UNIT
+// UNIT -OBJECT, VARIABLES - PROPERTY, FUNCTIONS - METHODS
 
-const friends = ["jojo", "peter", "bob"];
-localStorage.setItem("friends", friends);
-localStorage.clear();
+const baseSalary = 30000;
+const overtime = 20;
+const rate = 20;
 
-// localStorage.setItem("friends", friends); // VALUES STORED AS A STRING, NOT ARRAY
+// HAVE TO PASS PARAMETERS IN FUNCTION HERE
+function getWage(cashInHand, overtime, rate) { // PROCEDURAL
+    return cashInHand + (overtime * rate);
+};
 
-localStorage.setItem("friends", JSON.stringify(friends));
-
-const values = JSON.parse(localStorage.getItem("friends"));
-console.log(values[0]);
-
-let fruits;
-
-if (localStorage.getItem("fruits")) {
-    fruits = JSON.parse(localStorage.getItem("fruits"));
-} else {
-    fruits = [];
-}
-
-console.log(fruits);
-// fruits.push("apple");
-fruits.push("banana")
-localStorage.setItem("fruits", JSON.stringify(fruits));
+// ENCAPSULATION - COMBINING VARIABLES AND RELATED FUNCTIONS INTO A SINGLE UNIT, AN OBJECT
+const employee = {
+    baseSalary: 30000,
+    overtime: 10,
+    rate: 20,
+    getWage: function () { // EVALUATES TO WHATEVER IS RETURNED
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+};
+const ctc = employee.getWage();
+console.log(`ctc of employee is ${ctc}.`);
