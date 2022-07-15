0eae006438ae8de72a66d13fef80f8b28dd5f5d2
Rahul <rahul.pandita41299@gmail.com>
2022-07-15 15:47:23 +0530

    RN - array concat

--- js/main.js
+++ js/main.js
@@ -1,26 +1,7 @@
-// ARRAY FILTER() METHOD - CREATES NEW ARRAY IF ITEM PASSES VALUE
-// FILTER RETURNS TRUE OR FALSE - IF TRUE - THEN ELEMENT PASSED ONTO NEW ARRAY
+// ARRAY CONCAT() METHOD
 
-let numbers = [23, 33, 42, 79, 76, 77, 55, 49, 2, 3, 5, 6];
-let breakPoint = 30;
+let a = [2, 4, 5, 7, 8];
+let b = ["jkj", 9, 0909, "popo"];
 
-// IF NUMBER LESS THAN 30, PASS ONTO NEW ARRAY
-let smallNumbers = numbers.filter((item, index, array) => {
-    return item < 30; // CHECKS EVERY ITEM IN ARRAY AGAINST THIS CONDITION.
-    // if true, element passed onto (returned) new array
-});
-console.log(smallNumbers);
-
-// IF NUMBER GREATER THAN 30..
-let bigNumbers = numbers.filter((item, index, array) => {
-    return item > 30;
-})
-console.log(bigNumbers);
-
-// IF NUMBER IS ODD
-let odd = numbers.filter((item, index, array) => {
-    return (item % 2) !== 0;
-});
-console.log(odd);
-
-
+let nct = a.concat(b);
+console.log(nct);
