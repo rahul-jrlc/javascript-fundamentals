fb4976717009031e5a22538a0dbab33ffefa31f5
Rahul <rahul.pandita41299@gmail.com>
2022-07-15 15:43:35 +0530

    RN - filter() method

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-// ARRAY MAP() METHOD
-// CREATES A NEW ARRAY POPULATED WITH RESULTS OF CALLING A CALLBACK FUNC ON EVERY ELEMENT IN THE CALLING ARRAY
+// ARRAY FILTER() METHOD - CREATES NEW ARRAY IF ITEM PASSES VALUE
+// FILTER RETURNS TRUE OR FALSE - IF TRUE - THEN ELEMENT PASSED ONTO NEW ARRAY
 
-// CALLBACK FUNCTION IN MAP ACCEPTS SAME ARGUMENTS AS FOR EACH CALLBACK FN, 
-// THE CURRENT ELEMENT BEING PROCESSED IN THE ARRAY (VALUE/ITEM), THE INDEX OF CURRENT ELEMENT AND THE ENTIRE ARRAY ON WHICH MAP WAS CALLED.
+let numbers = [23, 33, 42, 79, 76, 77, 55, 49, 2, 3, 5, 6];
+let breakPoint = 30;
 
-let myArr = ["one", "two", "three", "four", "five", "six", "seven", "eight"];
-
-// determine length of each str and save it in an array
-
-let strLength = myArr.map(function (item, index, array) {
-    return item.length; // RETURNS NEW ARRAY WITH EACH ELEMENT BEING THE RESULT OF THE CALLBACK FUNCTION
+// IF NUMBER LESS THAN 30, PASS ONTO NEW ARRAY
+let smallNumbers = numbers.filter((item, index, array) => {
+    return item < 30; // CHECKS EVERY ITEM IN ARRAY AGAINST THIS CONDITION.
+    // if true, element passed onto (returned) new array
 });
+console.log(smallNumbers);
 
-console.log(strLength);
+// IF NUMBER GREATER THAN 30..
+let bigNumbers = numbers.filter((item, index, array) => {
+    return item > 30;
+})
+console.log(bigNumbers);
+
+// IF NUMBER IS ODD
+let odd = numbers.filter((item, index, array) => {
+    return (item % 2) !== 0;
+});
+console.log(odd);
+
 
