114dd73b6e0402b5d1bc65b1e3b5ee8e8b6ce86a
Rahul <rahul.pandita41299@gmail.com>
2022-07-16 18:49:08 +0530

    working with JSON

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
-// FUNCTIONAL PROGRAMMING
-// HIGHER ORDER FUNCTIONS
-// FUNCTIONS ARE FIRST CLASS OBJECTS - VALUES JUST LIKE STRINGS OR NUMBERS.
-// FUNCTIONS CAN BE ASSIGNED TO VARIABLES OR PASSED INTO OTHER FUNCTIONS(HIGHER ORDER FUNCTIONS)
-// RESULT IN SHORTER CODE DUE TO COMPOSITION OF FUNCTIONS, LESS LOGIC
+// JSON - JAVASCRIPT OBJECT NOTATION
+// LIGHTWEIGHT DATA INTERCHANGE FORMAT
+// USED TO SEND DATA BACK AND FORTH TO A SERVER
 
-let myArr = [{ age: 33 },
-    { age: 98 },
-    { age: 21 },
-    { age: 65 }];
-    
-// GET SUM OF ALL AGES
-
-// USING FOR LOOP
-
-let ageCount = 0;
-for (let i = 0; i < myArr.length; i++) {
-    ageCount += myArr[i].age;
+let person = {
+    name: "popo",
+    age: 76,
+    address: {
+        street: 77,
+        city: "gurgaon"
+    },
+    children: ["momo", "lolo"]
 };
-console.log(ageCount);
 
-// USING REDUCE
-let totalAge = myArr.reduce(function (prevVal, curVal, index) {
-    console.log(prevVal, curVal);
-    return prevVal + curVal.age;
-}, 0)
-console.log(totalAge);
+// console.log(person.children[0]);
+// console.log(person.address.city);
+
+let people = [
+    { name: "john", age: 23 }, { name: "poiu", age: 31 }, { name: "tom", age: 43 }
+];
+console.log(people[1].age);
+
+let arr = people.map(function (value, index, array) {
+    return value.name;
+});
+console.log(arr);
+
+// CHANGING OBJECT LITERAL TO A JSON STRING
+
+// let jstr = JSON.stringify(person);
+// console.log(jstr);
+
+// TURNING JSON STRING BACK TO OBJECT
+
+// jstr = JSON.parse(jstr);
+// console.log(jstr);
