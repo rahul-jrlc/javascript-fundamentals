8c8c87cedc60c0b6fd7fcb547eee07ad15172bd2
Rahul <rahul.pandita41299@gmail.com>
2022-07-16 13:46:55 +0530

    Functional programming - higher order functions - map()

--- js/main.js
+++ js/main.js
@@ -2,35 +2,24 @@
 // HIGHER ORDER FUNCTIONS
 // FUNCTIONS ARE FIRST CLASS OBJECTS - VALUES JUST LIKE STRINGS OR NUMBERS.
 // FUNCTIONS CAN BE ASSIGNED TO VARIABLES OR PASSED INTO OTHER FUNCTIONS(HIGHER ORDER FUNCTIONS)
-
-
-
-let triple = function (x) { // CREATE AN ANONYMOUS FUNC, ASSIGN TO A VARIABLE
-    return x * 3;
-};
-
-let pop = triple;
+// RESULT IN SHORTER CODE DUE TO COMPOSITION OF FUNCTIONS, LESS LOGIC
 
 let myArr = [{ name: "bobo", age: 33 },
     { name: "popo", age: 98 },
     { name: "lolo", age: 21 },
     { name: "chintu", age: 65 }];
     
-// FILTER OUT NAMES WITH AGE  GREATER THAN 35
-// NORMAL FOR LOOP
+// GET ARRAY OF ALL THE NAMES OF ALL THE ANIMALS
+// USING FOR LOOP
 
-let old = [];
+let personName = [];
 for (let i = 0; i < myArr.length; i++) {
-    if (myArr[i].age > 35) {
-        old.push(myArr[i]);
-    } 
+    personName.push(myArr[i].name);
 };
-console.log(old);
+console.log(personName);
 
-// USING FILTER() - HIGHER ORDER FUNCTION THAT ACCEPTS A CALLBACK FUNCTION
-// RETURNS TO NEW ARRAY IF RETURN STATEMENT HOLDS TRUE FOR ELEMENT BEING ITERATED
-let buddhe = myArr.filter(function (value, index, array) {
-    return value.age > 35;
-});
-console.log(buddhe);
+// USING MAP
+// CALLS THE CALLBACK FUNCTION ONCE FOR EACH ELEMENT IN ARRAY, DOES WHATEVER SPECIFIED IN CALLBACK FUNCTION - RETURNS TRANSFORMED OBJECT
 
+let nameOfPerson = myArr.map((value, index, array) => value.name );
+console.log(nameOfPerson);
